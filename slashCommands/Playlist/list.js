3832edd3ec866b901e652be6b8c09642cc8afb56
plantedpurpose11 <plantedpurpose59@gmail.com>
2026-05-21 19:53:27 -0400

    fix: ensure infos key exists before playlist read in slashCommands/Playlist/list.js

--- slashCommands/Playlist/list.js
+++ slashCommands/Playlist/list.js
@@ -15,6 +15,8 @@ const { MessageEmbed } = require("discord.js");
       try {
         const { guildId } = interaction;
         const nameArg = interaction.options.getString("name");
+        client.infos.ensure(guildId, {});
+
         const allPlaylists = client.infos.get(guildId, "playlists") || {};
 
         if (!nameArg) {
