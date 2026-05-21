73a1f8ebdbb82dc6d2e9777ee76796f9de7df560
plantedpurpose11 <plantedpurpose59@gmail.com>
2026-05-21 18:03:00 -0400

    feat: add prefix command defaultvolume

--- commands/Settings/defaultvolume.js
+++ commands/Settings/defaultvolume.js
@@ -1,70 +1,22 @@
-const {
-  MessageEmbed
-} = require("discord.js");
-const config = require("../../botconfig/config.json");
-const ee = require("../../botconfig/embed.json");
-const settings = require("../../botconfig/settings.json");
-module.exports = {
-  name: "defaultvolume", //the command name for execution & for helpcmd [OPTIONAL]
-  category: "Settings",
-  aliases: ["dvolume"],
-  usage: "defaultvolume <Percentage>",
-  cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
-  description: "Defines the Default Volume of the Bot!", //the command description for helpcmd [OPTIONAL]
-  memberpermissions: ["MANAGE_GUILD "], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
-  requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
-  alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
-  run: async (client, message, args) => {
-    try {
-      //things u can directly access in an interaction!
-      const {
-        member,
-      } = message;
-      const {
-        guild
-      } = member;
-      if (!args[0]) {
-        return message.reply({
-          embeds: [
-            new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter({ text: ee.footertext, iconURL: ee.footericon })
-            .setTitle(`${client.allEmojis.x} **Please add a Volume!**`)
-            .setDescription(`**Usage:**\n> \`${client.settings.get(guild.id, "prefix")}defaultvolume <percentage>\``)
-          ],
-        })
-      }
-      let volume = Number(args[0]);
-      if (!volume || (volume > 150 || volume < 1)) {
-        return message.reply({
-          embeds: [
-            new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter({ text: ee.footertext, iconURL: ee.footericon })
-            .setTitle(`${client.allEmojis.x} **The Volume __must__ be between \`1\` and \`150\`!**`)
-          ],
-        })
-      }
-      client.settings.set(guild.id, volume, "defaultvolume");
-      return message.reply({
-        embeds: [
-          new MessageEmbed()
-          .setColor(ee.color)
-          .setFooter({ text: ee.footertext, iconURL: ee.footericon })
-          .setTitle(`${client.allEmojis.check_mark} **The Default-Volume has been set to: \`${volume}\`!**`)
-        ],
-      })
-    } catch (e) {
-      console.log(String(e.stack).bgRed)
+const { MessageEmbed } = require("discord.js");
+  const ee = require("../../botconfig/embed.json");
+  module.exports = {
+    name: "defaultvolume",
+    category: "Settings",
+    aliases: ["setvolume","defvol"],
+    usage: "defaultvolume <1-150>",
+    cooldown: 1,
+    description: "Sets the default volume (1-150)",
+    memberpermissions: ["MANAGE_GUILD"],
+    requiredroles: [],
+    alloweduserids: [],
+    run: async (client, message, args) => {
+      try {
+        const volume = parseInt(args[0]);
+        if (!args[0] || isNaN(volume) || volume < 1 || volume > 150)
+          return message.reply({ embeds: [new MessageEmbed().setColor(ee.wrongcolor).setFooter({ text: ee.footertext, iconURL: ee.footericon }).setTitle(`${client.allEmojis.x} **Volume must be between \`1\` and \`150\`!**`)] });
+        client.settings.set(message.guild.id, volume, "defaultvolume");
+        return message.reply({ embeds: [new MessageEmbed().setColor(ee.color).setFooter({ text: ee.footertext, iconURL: ee.footericon }).setTitle(`${client.allEmojis.check_mark} **Default volume set to \`${volume}\`!**`)] });
+      } catch (e) { console.log(String(e.stack).bgRed); }
     }
-  }
-}
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template
- * @INFO
- * Work for Milrato Development | https://milrato.eu
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
+  };
