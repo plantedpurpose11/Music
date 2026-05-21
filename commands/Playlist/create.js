e53c11f498f426615987a43b31404dad504fde7e
plantedpurpose11 <plantedpurpose59@gmail.com>
2026-05-21 19:53:31 -0400

    fix: ensure infos key exists before playlist read in commands/Playlist/create.js

--- commands/Playlist/create.js
+++ commands/Playlist/create.js
@@ -23,6 +23,8 @@ const { MessageEmbed } = require("discord.js");
         const mapTrack = t => { if (!t?.info) return null; return { info: { identifier: t.info.identifier||"", uri: t.info.uri||"", title: t.info.title||"Unknown", author: t.info.author||"Unknown", length: t.info.length||0, isStream: t.info.isStream||false }, requester: { id: t.requester?.id, tag: t.requester?.user?.tag||t.requester?.tag } }; };
         const tracks = [...(hasCurrent ? [mapTrack(player.queue.current)].filter(Boolean) : []), ...(player.queue.tracks||[]).map(mapTrack).filter(Boolean)];
         if (!tracks.length) return message.reply({ embeds: [new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Queue is empty!**`)] });
+        client.infos.ensure(guild\.id, {});
+
         const allPlaylists = client.infos.get(guild.id, "playlists") || {};
         if (allPlaylists[playlistName.toLowerCase()]) return message.reply({ embeds: [new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **A playlist named "${playlistName}" already exists!**`)] });
         allPlaylists[playlistName.toLowerCase()] = { name: playlistName, tracks, author: { id: member.id, tag: member.user.tag }, createdAt: Date.now() };
