3b8f47fcd452711fc6eadcd543368957eab3de3a
plantedpurpose11 <plantedpurpose59@gmail.com>
2026-05-21 19:53:33 -0400

    fix: ensure infos key exists before playlist read in commands/Playlist/remove.js

--- commands/Playlist/remove.js
+++ commands/Playlist/remove.js
@@ -16,6 +16,8 @@ const { MessageEmbed } = require("discord.js");
         const playlistName = args[0]?.toLowerCase();
         const position     = parseInt(args[1]);
         if (!playlistName || isNaN(position)) return message.reply({ embeds: [new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Usage: \`playlistremove <name> <position>\`**`)] });
+        client.infos.ensure(guild\.id, {});
+
         const allPlaylists = client.infos.get(guild.id, "playlists") || {};
         const playlist = allPlaylists[playlistName];
         if (!playlist) return message.reply({ embeds: [new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Playlist not found!**`).setDescription(`Available: \`${Object.keys(allPlaylists).join(", ")||"None"}\``)] });
